8201c0d7ab422b4d7ec35245adcfac0be84c87b2
JuanSAlvarez <juan.alvarez1@ucalgary.ca>
2019-07-12 11:23:24 -0600

    Working temp mechanism

--- ScaleUp/TempToKVal.m
+++ ScaleUp/TempToKVal.m
@@ -5,28 +5,48 @@
 % since July 11th 2019
 % changes: Jean-David Rousseau
 
-function [k] = TempToKVal(~)
-% hardcoded values cuz tests
-Tmin = 303;
-Tmax = 333;
-Tswitch = 323;
-Ea = 400;
-A = 1;
-kd = -5.85154*10^-6;
-Eo = 6;
-R = 8.314;
-t = 1000;
+% function [k] = TempToKVal(~)
+% % hardcoded values cuz tests
+% Tmin = 303;
+% Tmax = 333;
+% Tswitch = 323;
+% Ea = 400;
+% A = 1;
+% kd = -5.85154*10^-6;
+% Eo = 6;
+% R = 8.314;
+% t = 1000;
+% 
+% % growth
+% Tg = linspace(Tmin,Tswitch);
+% v1 = A*Eo*exp(-Ea./(R.*Tg)); 
+% % decay
+% Td = linspace(Tswitch,Tmax);
+% v2 = A*Eo*exp(-Ea./(R.*Td)+kd*t); 
+% 
+% figure
+% plot(Tg,v1,Td,v2);
+% xlabel('Temperature');
+% ylabel('Vmax'); 
+% title('Enzyme at temp'); 
+% end
 
-% growth
-Tg = linspace(Tmin,Tswitch);
-v1 = A*Eo*exp(-Ea./(R.*Tg)); 
-% decay
-Td = linspace(Tswitch,Tmax);
-v2 = A*Eo*exp(-Ea./(R.*Td)+kd*t); 
+% xrun = [0 10000];
+% work = 6*exp(-((72.17*xrun.^-1)+(200./(exp(-(288.67.*xrun.^-1))))));
+% plot(xrun,work,'r'); 
 
-figure
-plot(Tg,v1,Td,v2);
-xlabel('Temperature');
-ylabel('Vmax'); 
-title('Enzyme at temp'); 
-end
+clc; 
+T = linspace(273,600);
+t = 600; 
+R = 1.987E-3; %check this
+Eo = 10;
+Ed = 5; 
+Ad = 5; 
+Ea = 4; 
+Aa = 1;
+
+kd = Ad*exp(-Ed./(T.*R));
+E = Eo*exp(-kd*t);
+v = Aa*E.*exp(-Ea./(T.*R));
+plot(T,v,'r'); 
+
